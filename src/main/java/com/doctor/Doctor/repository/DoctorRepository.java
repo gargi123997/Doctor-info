632969d6d2eef15b848ee2e0bff081f8d9748984
gargi123997 <gargibareo7@gmail.com>
2026-02-25 20:36:49 -0800

    changes

--- src/main/java/com/doctor/Doctor/repository/DoctorRepository.java
+++ src/main/java/com/doctor/Doctor/repository/DoctorRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+//Only doctorgit
 public interface DoctorRepository extends JpaRepository<Doctor,Long> {
 }
